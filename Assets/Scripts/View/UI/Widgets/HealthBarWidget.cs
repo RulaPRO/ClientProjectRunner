91e148e1f9c950796b73d6b31b542008673b85cf
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-21 23:38:59 +0300

    HealthBarWidget

---
+++ Assets/Scripts/View/UI/Widgets/HealthBarWidget.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace View.UI.Widgets
+{
+    public class HealthBarWidget : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] hearts;
+
+        private void OnEnable()
+        {
+            UpdateHealthValue(SceneContext.I.PlayerService.Health.Value);
+
+            SceneContext.I.PlayerService.Health.OnHealthIncrease += UpdateHealthValue;
+            SceneContext.I.PlayerService.Health.OnHealthDecrease += UpdateHealthValue;
+        }
+
+        private void OnDisable()
+        {
+            SceneContext.I.PlayerService.Health.OnHealthIncrease -= UpdateHealthValue;
+            SceneContext.I.PlayerService.Health.OnHealthDecrease -= UpdateHealthValue;
+        }
+
+        private void UpdateHealthValue(int value)
+        {
+            for (var i = 0; i < hearts.Length; i++)
+            {
+                hearts[i].SetActive(value > i);
+            }
+        }
+    }
+}
