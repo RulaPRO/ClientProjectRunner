5fb03a1420141b3a2ce46954b38ca33c13efa9e4
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-21 23:37:23 +0300

    UIFactory

---
+++ Assets/Scripts/Factories/UIFactory.cs
@@ -0,0 +1,16 @@
+﻿using Services.UIService.Implementation.UIUnits;
+using UnityEngine;
+
+namespace Factories
+{
+    public class UIFactory
+    {
+        public TScreen CreateScreen<TScreen>(Transform parent) where TScreen : UIScreen
+        {
+            var screenPrefab = Resources.Load<TScreen>($"UI/Screens/{typeof(TScreen).Name}");
+            var instance = Object.Instantiate(screenPrefab, parent);
+
+            return instance;
+        }
+    }
+}
