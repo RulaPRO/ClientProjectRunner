d9a394459e4b6f8ccb9f99f53e1c1e42c4e64fb8
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-21 23:30:39 +0300

    SpeedController

---
+++ Assets/Scripts/Services/Player/SpeedController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Services.Player
+{
+    public class SpeedController
+    {
+        public Action<float> OnMaxSpeedChange;
+
+        public float SpeedDefault { get; private set; }
+        public float SpeedMax { get; private set; }
+        public float Acceleration { get; private set; }
+
+        public void Init()
+        {
+            SpeedDefault = SceneContext.I.Config.BaseSpeed;
+            SpeedMax = SpeedDefault;
+            Acceleration = SceneContext.I.Config.Acceleration;
+        }
+
+        public void IncreaseMaxSpeed(float value)
+        {
+            SpeedMax += value;
+
+            OnMaxSpeedChange?.Invoke(SpeedMax);
+        }
+    }
+}
