216b33aed87e59309e3a1567ceb8e202e20d7ac6
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-21 23:36:32 +0300

    HealthController

---
+++ Assets/Scripts/Services/Player/HealthController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Services.Player
+{
+    public class HealthController
+    {
+        public Action<int> OnHealthIncrease;
+        public Action<int> OnHealthDecrease;
+
+        public int Value { get; private set; }
+        public bool HasInvincibility { get; private set; }
+
+        public void Init(int healthValue)
+        {
+            Value = healthValue;
+
+            OnHealthIncrease?.Invoke(Value);
+        }
+
+        public void IncreaseHealth(int value)
+        {
+            Value = Mathf.Clamp(Value + value, 0, SceneContext.I.Config.BaseHealth);
+
+            OnHealthIncrease?.Invoke(Value);
+        }
+
+        public void DecreaseHealth()
+        {
+            if (HasInvincibility)
+            {
+                return;
+            }
+
+            Value--;
+
+            OnHealthDecrease?.Invoke(Value);
+        }
+
+        public async void EnableInvincibility(int value)
+        {
+            HasInvincibility = true;
+
+            await Task.Delay(value);
+
+            HasInvincibility = false;
+        }
+    }
+}
