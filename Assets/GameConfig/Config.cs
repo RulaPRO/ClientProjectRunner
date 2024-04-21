cfbd1e1a019beb862292acc67ac8fc3112c61dd5
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-21 23:19:24 +0300

    Config

---
+++ Assets/GameConfig/Config.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GameConfig
+{
+    [CreateAssetMenu(
+        menuName = "Config/" + nameof(Config),
+        fileName = nameof(Config))]
+    public class Config : ScriptableObject
+    {
+        public float BaseSpeed;
+        public float Acceleration;
+    }
+}
