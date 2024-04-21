dfac95c2b9e94177e709e9434ecf7b96e50e091e
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-21 23:38:17 +0300

    SceneContext

---
+++ Assets/Scripts/SceneContext.cs
@@ -0,0 +1,29 @@
+﻿using GameConfig;
+using Services;
+using Services.Player;
+using Services.Tiles;
+using Services.UIService.Implementation;
+using Services.UIService.Interfaces;
+using UnityEngine;
+
+public class SceneContext
+{
+    private static SceneContext i;
+    public static SceneContext I => i ??= new SceneContext();
+
+    public PlayerService PlayerService { get; }
+    public TileService TileService { get; }
+    public IUIService UIService { get; }
+    public AdsService AdsService { get; }
+    public Config Config { get; }
+
+    private SceneContext()
+    {
+        Config = Resources.Load<Config>("Config");
+
+        PlayerService = new PlayerService();
+        TileService = new TileService();
+        UIService = new UIService();
+        AdsService = new AdsService();
+    }
+}
