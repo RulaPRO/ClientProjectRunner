206677e2480f507d020ba45f8f51622ba9f0dd14
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-21 23:37:37 +0300

    BonusFactory

---
+++ Assets/Scripts/Factories/BonusFactory.cs
@@ -0,0 +1,22 @@
+﻿using Services.Tiles;
+using UnityEngine;
+using View.Bonuses;
+
+namespace Factories
+{
+    public class BonusFactory
+    {
+        public BonusView Create(BonusType bonusType)
+        {
+            var prefab = LoadBonusPrefab(bonusType);
+            var instance = Object.Instantiate(prefab);
+
+            return instance;
+        }
+
+        private BonusView LoadBonusPrefab(BonusType bonusType)
+        {
+            return Resources.Load<BonusView>($"Bonuses/PrefabBonus{bonusType}View");
+        }
+    }
+}
