5c6203ffb63a15a7cd1775438223a4394fa67ee4
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-22 11:55:10 +0300

    CameraBrain

---
+++ Assets/Scripts/CameraBrain.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraBrain : MonoBehaviour
+{
+    [SerializeField] private float distance = 10f;
+    [SerializeField] private float height = 5f;
+
+    private GameObject target;
+    private float currentRotation;
+        
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+            
+        var targetPosition = target.transform.position;
+        var position = targetPosition - (Vector3.forward * distance);
+        position.y = targetPosition.y + height;
+        transform.position = position;
+        transform.LookAt(targetPosition);
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+}
