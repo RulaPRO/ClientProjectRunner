75cb4ddd51fe2351617aede19ef1ec6a6f9b2c2c
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-22 13:03:48 +0300

    UI Elements

---
+++ Assets/Scripts/View/UI/Elements/CompletedTileTypeInfoUI.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace View.UI.Elements
+{
+    public class CompletedTileTypeInfoUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI label;
+
+        public void SetLabel(string text)
+        {
+            label.text = text;
+        }
+    }
+}
---
+++ Assets/Scripts/View/UI/Elements/CompletedTilesListUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace View.UI.Elements
+{
+    public class CompletedTilesListUI : MonoBehaviour
+    {
+        [SerializeField] private CompletedTileTypeInfoUI prefabCompletedTileTypeInfoUI;
+        [SerializeField] private GameObject content;
+
+        private void OnEnable()
+        {
+            var finishedTiles = SceneContext.I.TileService.FinishedTiles;
+            foreach (var pair in finishedTiles)
+            {
+                var instance = Instantiate(prefabCompletedTileTypeInfoUI, content.transform);
+                instance.SetLabel($"{pair.Key.ToString()} - {pair.Value.ToString()}");
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (Transform child in content.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
