c91d862ce458fec1bdb190afa1a3bedbd318c8b7
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-04-22 11:28:52 +0300

    PlayerFactory

---
+++ Assets/Scripts/Factories/PlayerFactory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Factories
+{
+    public class PlayerFactory
+    {
+        public PlayerView Create()
+        {
+            var prefab = LoadPlayerPrefab("PrefabPlayer");
+            var instance = Object.Instantiate(prefab);
+
+            return instance;
+        }
+
+        private PlayerView LoadPlayerPrefab(string id)
+        {
+            return Resources.Load<PlayerView>($"Player/{id}");
+        }
+    }
+}
